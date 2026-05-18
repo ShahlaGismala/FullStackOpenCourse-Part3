33dd380b36a62562df851b28206c2910b97b6fa2
shahla <kokoshahla@gmail.com>
2026-05-18 12:14:02 +0300

    part3

---
+++ phonebook/step3/index.js
@@ -0,0 +1,56 @@
+const express = require('express')
+const app = express()
+
+let persons = [
+    { 
+      "id": "1",
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": "2",
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": "3",
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": "4",
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+app.get('/', (request, response) => {
+  response.send('<h1>Hello There!</h1>')
+})
+
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+  const id = req.params.id;
+  const person = persons.find(p => p.id === id);
+
+  if (person) {
+    res.json(person);
+  } else {
+    res.status(404).end();
+  }
+});
+
+
+app.get('/info', (req, res) => {
+  const count = persons.length;
+  const date = new Date();
+  res.send(`Phonebook has info for ${count} people<br>${date}`);
+});
+
+const PORT = 3003
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
---
+++ phonebook/step9/index.js
@@ -0,0 +1,100 @@
+const morgan = require('morgan');
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+morgan.token('body', (req) => JSON.stringify(req.body));
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
+
+
+let persons = [
+    { 
+      "id": "1",
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": "2",
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": "3",
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": "4",
+      "name": "Sara Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+app.get('/', (request, response) => {
+  response.send('<h1>Hello There!</h1>')
+})
+
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+  const id = req.params.id;
+  const person = persons.find(p => p.id === id);
+
+  if (person) {
+    res.json(person);
+  } else {
+    res.status(404).end();
+  }
+});
+
+app.get('/info', (req, res) => {
+  const count = persons.length;
+  const date = new Date();
+  res.send(`Phonebook has info for ${count} people<br>${date}`);
+});
+
+app.delete('/api/persons/:id', (req, res) => {
+  const id = req.params.id;
+  const index = persons.findIndex(p => p.id === id);
+
+  if (index !== -1) {
+    persons.splice(index, 1);  
+    res.status(204).end();      
+  } else {
+    res.status(404).end();      
+  }
+});
+
+app.post('/api/persons', (req, res) => {
+  const body = req.body;
+  console.log(req.body);
+
+  if (!body.name || !body.number) {
+    return res.status(400).json({ error: 'name or number missing' });
+  }
+
+  const nameExists = persons.some(p => p.name === body.name);
+  if (nameExists) {
+    return res.status(400).json({ error: 'name must be unique' });
+  }
+
+  const person = {
+    id: Math.floor(Math.random() * 1000000).toString(),
+    name: body.name,
+    number: body.number
+  };
+
+  persons.push(person);
+  res.json(person);
+});
+
+
+const PORT = 3009
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})   
